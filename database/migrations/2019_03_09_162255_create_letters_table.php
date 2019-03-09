72994cf8985cd93d1a88fe88559ddd71ff2f1424
vladimirbabaskin <vladimirbabaskin@gmail.com>
2019-03-09 20:36:31 +0200

    init

---
+++ database/migrations/2019_03_09_162255_create_letters_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLettersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('letters', function (Blueprint $table) {
+            $table->string("title", 127);
+            $table->text("text");
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('letters');
+    }
+}
